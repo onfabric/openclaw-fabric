5392bd3e2e714d09fd638ece9707245b6d15750a
ilbertt <luca@onfabric.io>
2026-03-24 16:42:48 +0000

    refactor: const

--- src/crons/deep-user-profile.ts
+++ src/crons/deep-user-profile.ts
@@ -4,10 +4,12 @@ import { getSkillPath, Skill } from '../lib/skills';
 const EVERY_DAY_AT_MIDNIGHT = '0 0 * * *';
 
 function register() {
+  const deepUserProfileSkill = getSkillPath(Skill.DEEP_USER_PROFILE);
+
   cli.cron.add({
     cronExpression: EVERY_DAY_AT_MIDNIGHT,
     name: 'fabric-deep-user-profile-cron',
-    prompt: `Use the ${getSkillPath(Skill.DEEP_USER_PROFILE)} skill to update the user profile.`,
+    prompt: `Use the ${deepUserProfileSkill} skill to update the user profile.`,
   });
 }
 
